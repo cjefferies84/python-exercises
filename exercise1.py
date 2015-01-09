f3ea96360335100b9f384dc3023d27d6a5b2c5ad
Chris Jefferies <cjefferies8439@gmail.com>
2015-01-09 12:18:07 -0700

    Cleaned up python exercises

--- exercise1.py
+++ exercise1.py
@@ -1,48 +1,11 @@
-# first_num = raw_input("Please enter the first number: ")
-# second_num = raw_input("Please enter the second number: ")
-# if first_num.is_digit() or second_num.is_digit():
-#         print "Both integers should be numbers, not something else!"
-#         quit()
-#
-# num1 = int(first_num)
-# num2 = int(second_num)
-# if num2 == 0:  # second number input cannot equal zero
-#         print "The second number would cause a divide by zero exception!"
-#         quit()
-#
-# print "The sum of %d and %d is: %d" %(num1, num2, num1 + num2)
-# print "The difference of %d and %d is: %d" %(num1, num2, num1 - num2)
-# print "The product of %d and %d is: %d" %(num1, num2, num1 * num2)
-# print "The quotient of %d and %d is: %d with remainder %d" %(num1, num2, num1 / num2, num1 % num2)
-#
-#
-# def get_input():
-#         user_input = raw_input("Please enter the " + input_count + "")
-# second_num = raw_input("Please enter the second integer: ")
-# if first_num.is_digit() or second_num_.is_digit():
-#         print "Both integers should be numbers, not something else!"
-#         quit()
-#
-# num1 = int(num1_string)
-# num2 = int(num2_string)
-# if num2 == 0:  # second number input cannot equal zero
-#         print "The second number would cause a divide by zero exception!"
-#         quit()
-#
-# print "The sum of %d and %d is: %d" %(num1, num2, num1 + num2)
-# print "The difference of %d and %d is: %d" %(num1, num2, num1 - num2)
-# print "The product of %d and %d is: %d" %(num1, num2, num1 * num2)
-# print "The quotient of %d and %d is: %d with remainder %d" %(num1, num2, num1 / num2, num1 % num
-
-
 first_num = raw_input("Please enter the first integer: ")
 second_num = raw_input("Please enter the second integer: ")
-if first_num.is_digit() or second_num_.is_digit():
+if first_num.is_digit() or second_num.is_digit():
         print "Both integers should be numbers, not something else!"
         quit()
 
-num1 = int(num1_string)
-num2 = int(num2_string)
+num1 = int(first_num)
+num2 = int(second_num)
 if num2 == 0:  # second number input cannot equal zero
         print "The second number would cause a divide by zero exception!"
         quit()
@@ -51,56 +14,3 @@ print "The sum of %d and %d is: %d" %(num1, num2, num1 + num2)
 print "The difference of %d and %d is: %d" %(num1, num2, num1 - num2)
 print "The product of %d and %d is: %d" %(num1, num2, num1 * num2)
 print "The quotient of %d and %d is: %d with remainder %d" %(num1, num2, num1 / num2, num1 % num2)
-
-
-
-
-
-
-
-#user_input =
-#gall_conversion = input_char
-try:
-    gall_conversion = float(raw_input("Please enter number of gallons: "))
-except ValueError, e:
-    input_char = gall_conversion
-    while input_char == str(gallon_conversion):
-        print "Sorry, but you must enter a number value, please enter another value"
-    quit()
-print "The original number of gallons is: " + str(gall_conversion)
-liters_conversion = " gallons is the equivalent of " + str(gall_conversion * 3.07854117847) + " liters of gas"
-barrels_conversion = " gallons of gasoline requires " + str(gall_conversion * 19.5) + " barrels of oil"
-carb_diox_conversion = " gallons of gasoline produces " + str(gall_conversion * 20) + " pounds of CO2"
-ethanol_conversion = " gallons of gasoline produces " + str(gall_conversion * 75700) + " British Thermal Units"
-cost_per_gall = " gallons of gasoline requires $" + str(gall_conversion * 4) + " US dollars"
-print str(gall_conversion) + liters_conversion
-print str(gall_conversion) + barrels_conversion
-print str(gall_conversion) + carb_diox_conversion
-print str(gall_conversion) + ethanol_conversion
-print str(gall_conversion) + cost_per_gall
-print "Thanks for taking part in our conversion application"
-
-
-
-
-
-
-#
-# while True:
-#     gall_conversion = float(raw_input("Please enter number of gallons: "))
-# try:
-#     gall_conversion = float(my_float)
-# except ValueError, is not is_digit
-#         print e
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ test_1/test_2.py
@@ -0,0 +1,51 @@
+
+
+while True:
+    my_float = float(raw_input("Enter a float: "))
+
+    try:
+    my_float = float(my_float)
+    except Exception as e:
+        print e
+        print "That is not a float!"
+
+    print type(my_float)
+
+
+
+x = None
+# used to hold a value of later use
+
+File I/O # lecture notes
+
+
+# always use the with statement with opening files
+with open("text.txt", 'r') as f: #opening a txt file and retrieving context of file with automatically
+    for line in f:               #closes the file for you when you leave the with scope
+        print line
+print f
+
+# refer to modes of opening a file on lecture notes
+#
+#tuples are immutable - tuples are faster because they acutually hold the value itself, hence it does not have to go into memeory to find the value like lists
+#lists in python are mutable - convenient but slower because they point to the address in memory that hold a value and have to retrieve the value
+
+dictionary
+
+d = {
+    1: "one",
+    2: "two",
+    3: "three"
+}
+
+print d
+
+#this will print {1: 'one', 2: 'two', 3: 'three'} for output
+
+# dictionaries do not use an index, they use a key, in the example above the key's are 1:, 2:, and 3:
+
+lists in python are very similar to stacks in other languages
+
+slicing
+- unique to python, this can be used with any collection (strings or lists) slicing is very useful when trying to pull out a subset of data
+stride - how many items are we traversing
