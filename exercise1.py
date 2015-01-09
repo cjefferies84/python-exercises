177f1023061f0dac0f08be79daa152f7178eaaee
Chris Jefferies <cjefferies8439@gmail.com>
2015-01-09 13:00:56 -0700

    changes for exercises 1 and 2

--- exercise1.py
+++ exercise1.py
@@ -14,3 +14,39 @@ print "The sum of %d and %d is: %d" %(num1, num2, num1 + num2)
 print "The difference of %d and %d is: %d" %(num1, num2, num1 - num2)
 print "The product of %d and %d is: %d" %(num1, num2, num1 * num2)
 print "The quotient of %d and %d is: %d with remainder %d" %(num1, num2, num1 / num2, num1 % num2)
+
+first_num = raw_input("Please enter the first number: ")
+second_num = raw_input("Please enter the second number: ")
+if first_num.is_digit() or second_num.is_digit():
+        print "Both integers should be numbers, not something else!"
+        quit()
+
+num1 = int(first_num)
+num2 = int(second_num)
+if num2 == 0:  # second number input cannot equal zero
+        print "The second number would cause a divide by zero exception!"
+        quit()
+
+print "The sum of %d and %d is: %d" %(num1, num2, num1 + num2)
+print "The difference of %d and %d is: %d" %(num1, num2, num1 - num2)
+print "The product of %d and %d is: %d" %(num1, num2, num1 * num2)
+print "The quotient of %d and %d is: %d with remainder %d" %(num1, num2, num1 / num2, num1 % num2)
+
+
+def get_input():
+        user_input = raw_input("Please enter the " + input_count + "")
+second_num = raw_input("Please enter the second integer: ")
+if first_num.is_digit() or second_num_.is_digit():
+        print "Both integers should be numbers, not something else!"
+        quit()
+
+num1 = int(num1_string)
+num2 = int(num2_string)
+if num2 == 0:  # second number input cannot equal zero
+        print "The second number would cause a divide by zero exception!"
+        quit()
+
+print "The sum of %d and %d is: %d" %(num1, num2, num1 + num2)
+print "The difference of %d and %d is: %d" %(num1, num2, num1 - num2)
+print "The product of %d and %d is: %d" %(num1, num2, num1 * num2)
+print "The quotient of %d and %d is: %d with remainder %d" %(num1, num2, num1 / num2, num1 % num
--- exercise2.py
+++ exercise2.py
@@ -1,45 +1,14 @@
-# first_num = raw_input("Please enter the first number: ")
-# second_num = raw_input("Please enter the second number: ")
-# if first_num.is_digit() or second_num.is_digit():
-#         print "Both integers should be numbers, not something else!"
-#         quit()
-#
-# num1 = int(first_num)
-# num2 = int(second_num)
-# if num2 == 0:  # second number input cannot equal zero
-#         print "The second number would cause a divide by zero exception!"
-#         quit()
-#
-# print "The sum of %d and %d is: %d" %(num1, num2, num1 + num2)
-# print "The difference of %d and %d is: %d" %(num1, num2, num1 - num2)
-# print "The product of %d and %d is: %d" %(num1, num2, num1 * num2)
-# print "The quotient of %d and %d is: %d with remainder %d" %(num1, num2, num1 / num2, num1 % num2)
-#
-#
-# def get_input():
-#         user_input = raw_input("Please enter the " + input_count + "")
-# second_num = raw_input("Please enter the second integer: ")
-# if first_num.is_digit() or second_num_.is_digit():
-#         print "Both integers should be numbers, not something else!"
-#         quit()
-#
-# num1 = int(num1_string)
-# num2 = int(num2_string)
-# if num2 == 0:  # second number input cannot equal zero
-#         print "The second number would cause a divide by zero exception!"
-#         quit()
-#
-# print "The sum of %d and %d is: %d" %(num1, num2, num1 + num2)
-# print "The difference of %d and %d is: %d" %(num1, num2, num1 - num2)
-# print "The product of %d and %d is: %d" %(num1, num2, num1 * num2)
-# print "The quotient of %d and %d is: %d with remainder %d" %(num1, num2, num1 / num2, num1 % num
 
 
 
-user_input = raw_input("Please enter number of gallons: ")
-if user_input.isdigit():
-    pass
-    # gall_conversion = float(raw_input("Please enter number of gallons: "))
+gall_conversion = float(raw_input("Please enter number of gallons: "))
+while user_input = raw_input("Please enter number of gallons: "):
+        if user_input.isdigit() or user_input != 0:
+            print float(raw_input("Please enter number of gallons: "))
+else:
+    user_input = raw_input("Please only enter a number: ")
+
+  # gall_conversion = float(raw_input("Please enter number of gallons: "))
     #
     #         print "Sorry, but you must enter a number value, please enter another value"
     #     quit()
@@ -54,9 +23,7 @@ if user_input.isdigit():
     # print str(gall_conversion) + carb_diox_conversion
     # print str(gall_conversion) + ethanol_conversion
     # print str(gall_conversion) + cost_per_gall
-    # print "Thanks for taking part in our conversion application"
-else:
-    user_input = raw_input("Please only enter a number: ")
+
 
 
 #
